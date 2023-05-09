37116922932c767d62ab3336ce59c467815e915b
Douglas Tardioli <douglastardioli48@gmail.com>
2023-05-09 11:52:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+const main = document.querySelector("main");
+const root = document.querySelector(":root");
+const input = document.querySelector("#input");
+const resultInput = document.querySelector("#result");
+
+const allowedKeys = [
+  "(",
+  ")",
+  "/",
+  "*",
+  "-",
+  "+",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+  "1",
+  "0",
+  ".",
+  "%",
+  " ",
+];
+
+document.querySelectorAll('.charKey').forEach((charKeyBtn) => {
+  charKeyBtn.addEventListener('click', () => {
+    const value = charKeyBtn.dataset.value
+    input.value += value 
+  })
+})
+
+document.querySelector('#clear').addEventListener('click', () => {
+  result.value = ''
+  input.value = ''
+ 
+  input.focus()
+})
+
+input.addEventListener("keydown", (ev) => {
+  ev.preventDefault();
+  if (allowedKeys.includes(ev.key)) {
+    input.value += ev.key;
+    return;
+  }
+
+  if (ev.key === "Backspace") {
+    input.value = input.value.slice(0, -1);
+  }
+
+  if (ev.key === "Enter") {
+    calculate();
+  }
+});
+
+
+// **** CALCULATE **** //
+document.querySelector('#equal').addEventListener('click', calculate)
+
+
+function calculate() {
+  resultInput.value = 'ERROR'
+  resultInput.classList.add('error')
+  const result = eval(input.value)
+  resultInput.value = result
+  resultInput.classList.remove('error')
+}
+
+
+// **** DARK and LIGHT MODE **** //
+document.querySelector('#themeSwitcher').addEventListener('click', () => {
+  if (main.dataset.theme === 'dark') {
+    root.style.setProperty('--bg-color', "#f1f5f9")
+    root.style.setProperty('--border-color', "#aaa")
+    root.style.setProperty('--font-color', "#212529")
+    root.style.setProperty('--primary-color', "#26834A")
+    root.style.setProperty('--bg-box-color', "#212529")
+    main.dataset.theme = 'light'
+  } else {
+    root.style.setProperty('--bg-color', "#212529")
+    root.style.setProperty('--border-color', "#666")
+    root.style.setProperty('--font-color', "#f1f5f9")
+    root.style.setProperty('--primary-color', "#25f575")
+    root.style.setProperty('--bg-box-color', "#161616")
+    main.dataset.theme = 'dark'
+  }
+})
+
+// **** COPIAR CALCULO **** //
+
+document.querySelector('#copyToClipboard').addEventListener('click', (ev) => {
+  const button = ev.currentTarget
+  if (button.innerText === 'Copy') {
+    button.innerText = 'Copied!'
+    button.classList.add('success')
+    navigator.clipboard.writeText(resultInput.value)
+  } else {
+    button.innerText = 'Copy'
+    button.classList.remove('success')
+  }
+})
+
+
